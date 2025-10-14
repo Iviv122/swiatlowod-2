e7d006e6316c2440751d0e67df5accfa10c23fdd
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-10-14 16:21:37 +0200

    dashed line

--- objects/components/wire.gd
+++ objects/components/wire.gd
@@ -1,45 +1,70 @@
 extends Line2D
 class_name Wire
 
-var mouse : Mouse
+var mouse: Mouse
 
-var start : ComponentInstance
-var finish : ComponentInstance
+var start: ComponentInstance
+var finish: ComponentInstance
 
-var tween : Tween
+var tween: Tween
 
-func set_start(comp : ComponentInstance):
+func _ready():
+	texture = load("res://raw_assets/dashedline.png")
+	texture_mode = Line2D.LINE_TEXTURE_TILE
+
+
+	var shaderMat = ShaderMaterial.new()
+	shaderMat.shader = load("res://shaders/panning.gdshader")
+	material = shaderMat
+
+func set_start(comp: ComponentInstance):
 	start = comp
 	add_point(comp.global_position - global_position)
 	add_point(mouse.global_position)
 
-func set_end(comp : ComponentInstance):
+func set_end(comp: ComponentInstance):
 	remove_point(1)
 	finish = comp
 	add_point(finish.global_position)
 
-	var l : Line2D = Line2D.new()
+	var l: Line2D = Line2D.new()
 	l.add_point(start.global_position)
 	l.add_point(finish.global_position)
+
+	l.texture = texture.duplicate()
+	l.texture_mode = Line2D.LINE_TEXTURE_TILE
+	l.material = material.duplicate()
+	l.z_index -= 1
 
 	connect_neighbours()
 
 	get_tree().root.add_child(l)
 	queue_free()
 
-func check_if_connected(another : ComponentInstance) -> bool:
+func check_if_connected(another: ComponentInstance) -> bool:
 	return start.has_neighbour(another)
 
 func give_error():
 	if tween:
 		tween.kill()
 	tween = create_tween()
-	tween.tween_property(self,"default_color",Color.RED,0.1).set_trans(Tween.TRANS_SPRING)
-	tween.tween_property(self,"default_color",Color.WHITE,0.1).set_delay(0.1).set_trans(Tween.TRANS_SPRING)
+
+	var shader_mat := material as ShaderMaterial
+
+	tween.tween_method(
+		func(x): shader_mat.set_shader_parameter("color", Color.RED * x),
+		0, 1, 0.1
+	).set_trans(Tween.TRANS_SPRING)
+
+	tween.tween_method(
+		func(x): shader_mat.set_shader_parameter("color", Color.WHITE * x),
+		0, 1, 0.1
+	).set_delay(0.1).set_trans(Tween.TRANS_SPRING)
+
 
 func connect_neighbours():
-	start.connect_neighbour(finish)	
+	start.connect_neighbour(finish)
 	finish.connect_neighbour(start)
 
 func _process(_delta):
-	points[1] = mouse.global_position-global_position
+	points[1] = mouse.global_position - global_position
